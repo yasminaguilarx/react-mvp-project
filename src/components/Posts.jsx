56100698b6482c41b777c3d14fc79b4c4f6dbd30
yasminaguilarx <yaguilar2001@gmail.com>
2023-07-20 18:15:47 -0500

    styling changes to buttons

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -23,10 +23,17 @@ function Posts({
               <h1>{post.post_title}</h1>
               <span>{post.created_at}</span>
               <p>{post.blog_post}</p>
-              <button onClick={() => handleUpdateClick(post.post_id)}>
+              <button
+                onClick={() => handleUpdateClick(post.post_id)}
+                style={{ borderRadius: "15px", width: "60px", height: "60px" }}
+              >
                 Update
               </button>
-              <DeleteBlog id={post.post_id} setPosts={setPosts} />
+              <DeleteBlog
+                id={post.post_id}
+                setPosts={setPosts}
+                style={{ borderRadius: "15px", width: "60px", height: "60px" }}
+              />
             </div>
           ))
         : updatingPostId && (
